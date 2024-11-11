8d684afc6813c054f782d7568bb39c4d2f2fb7c5
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-11-11 19:29:32 -0300

    Adicionando comando individual para gerenciar volume do bot

---
+++ src/commands/misc/volume.js
@@ -0,0 +1,43 @@
+const {Client, Interaction, ApplicationCommandOptionType, EmbedBuilder} = require('discord.js');
+
+const { QueryType, useQueue } = require('discord-player')
+
+module.exports =  {
+  name: 'volume',
+  description: 'individual command to manage voice channel volume',
+  /**
+   *  @param {Client} client
+   *  @param {Interaction} interaction
+  */
+  callback: async (client, interaction) => {
+    await interaction.deferReply();
+
+    const embed = new EmbedBuilder();
+    const queue = useQueue(interaction.guild);
+    let amount = interaction.options.getNumber('amount', false);
+
+    if(!amount)
+    {
+      await interaction.editReply({
+        ephemeral: true,
+        embeds: [embed.setDescription(`Current volume level is ${queue.node.volume}%!`)],
+      });
+      return;
+    }
+
+    queue.node.setVolume(amount);
+    
+    await interaction.editReply({embeds: [embed.setDescription(`Volume has been set to ${amount}%!`)]});
+  },
+
+  options: [
+    {
+      name: 'amount',
+      description: 'volume to play the song (0-100)',
+      type: ApplicationCommandOptionType.Number,
+      min_value: 0,
+      max_value: 100,
+      required: false
+    }
+  ]
+}
