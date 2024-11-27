947985ace9b2304c7756d412126f5c54afb1dda6
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-11-27 20:37:18 -0300

    Adicionando um reply para frase aleatoria

---
+++ src/inserts/nerdDetector.js
@@ -0,0 +1,38 @@
+const { Client, Message, AttachmentBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'nerdDetector',
+  description: 'quote the user with the nerdiest comment possible',
+  match: "",
+  type: "includes",
+
+  /**
+  *  @param {Client} client
+  *  @param {Message} message
+  */
+  callback: async (client, message) => {
+    if(message.author.bot) return;
+
+    // 3%
+    if(message.content?.length > 3 && Math.floor((Math.random() * 100) + 1) > 97) {
+      // 15%
+      const isFire = Math.floor((Math.random() * 100) + 1) > 85;
+
+      const file = new AttachmentBuilder(`gifs/${isFire ? 'fire' : 'nerd'}.gif`);
+      message.reply({
+        content: isFire ? `"${message.content.toUpperCase()}" 🗣️🗣️🗣️🔥🔥🔥` : `'${message.content.toLowerCase()}' ☝☝🤓`,
+        files: [file]
+      });
+    
+      // 1%
+      if(!isFire && Math.floor((Math.random() * 100) + 1) > 99)
+      {
+        message.reply({
+          ephemeral: true,
+          content: `((Sorry if you feel offended, nothing personal dude!))`,
+        });
+      }
+    
+    }
+  }
+}
