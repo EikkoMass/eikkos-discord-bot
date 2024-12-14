862c493308be079268d6f4fc06d3dfcbfd7544fa
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-12-13 23:47:39 -0300

    Comando para pesquisar jogos pt2

--- src/commands/games/game.js
+++ src/commands/games/game.js
@@ -2,53 +2,96 @@ const {ApplicationCommandOptionType, EmbedBuilder, Client, Interaction } = requi
 require('dotenv').config();
 
 module.exports =  {
-/**
- *  @param {Client} client
- *  @param {Interaction} interaction
-*/
   callback: async (client, interaction) => {
-    const query = interaction.options?.get('query').value;
-
-    const game = await fetch(`https://api.igdb.com/v4/games`, {
-      method: 'POST',
-      headers: {
-        'Client-ID': process.env.IGDB_CLIENT_ID,
-        'Authorization': `Bearer ${client.igdb.access_token}`
-      },
-      body: `fields *, artworks.*, game_engines.*, screenshots.*; where id = ${query};`
-    });
-    if(!game.ok)
+    switch(interaction.options.getSubcommand())
     {
-      interaction.reply('Game not found, try again later!');
-      return;
+      case 'search':
+        await search(client, interaction);
+        return;
+      default:
+        await interaction.reply({
+          ephemeral: true,
+          content: `Game command not found!`
+        });
+        return;
     }
-
-    const json = await game.json();
-    console.log(json);
-
-    // for(let i = 0; i < json[0].artworks.length; i++)
-    // {
-    //   console.log(`https://images.igdb.com/igdb/image/upload/t_logo_med/${json[0].cover}.png`)
-    // }
-
-    const embed = new EmbedBuilder()
-      .setTitle(json[0].name)
-      .setDescription(json[0].summary);
-      //.setThumbnail(`https://images.igdb.com/igdb/image/upload/t_logo_med/${json[0].cover}.png`)
-
-    interaction.reply({
-      embeds: [embed]
-    });
   },
   name: 'game',
   description: 'Command to search games // IGDB',
   options: [
     {
-      name: 'query',
-      description: 'the game name you want to search',
-      type: ApplicationCommandOptionType.Number,
-      required: true,
-      autocomplete: true
-    },
+      name: 'search',
+      description: 'tries to find a game',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'query',
+          description: 'the game name you want to search',
+          type: ApplicationCommandOptionType.Number,
+          required: true,
+          autocomplete: true
+        }
+      ]
+    }
   ]
+}
+
+/**
+ *  @param {Client} client
+ *  @param {Interaction} interaction
+*/
+async function search(client, interaction) {
+  const query = interaction.options?.get('query').value;
+
+  const res = await fetch(`https://api.igdb.com/v4/games`, {
+    method: 'POST',
+    headers: {
+      'Client-ID': process.env.IGDB_CLIENT_ID,
+      'Authorization': `Bearer ${client.igdb.access_token}`
+    },
+    body: `fields name, summary, genres.*, keywords.*, first_release_date, involved_companies.company.*, cover.*, screenshots.*; limit 1; where id = ${query};`
+  });
+
+  if(!res.ok)
+  {
+    console.log(games.statusText);
+    interaction.reply('Game not found, try again later!');
+    return;
+  }
+
+  const games = await res.json();
+
+  if(!games)
+  {
+    interaction.reply('Game not found, try again later!');
+    return;
+  }
+
+  const fields = [
+    {name: 'Keywords', value: (games[0].keywords.length > 10 ? games[0].keywords.splice(0, 10) : games[0].keywords).map(tag => tag.name).join(", ")},
+    {name: 'Release Date', value: (new Date(games[0].first_release_date * 1000)).toDateString(), inline: true},
+    {name: 'Genres', value: games[0].genres.map(tag => tag.name).join(", "), inline: true},
+    {name: 'Companies', value: games[0].involved_companies.map(tag => tag.company.name).join(", ")},
+  ];
+
+  const embed = new EmbedBuilder()
+    .setTitle(games[0].name)
+    .setDescription(games[0].summary)
+    .setFields(fields)
+
+    if(games[0].cover)
+    {
+      embed
+        .setThumbnail(`https://images.igdb.com/igdb/image/upload/t_cover_big_2x/${games[0].cover.image_id}.png`);
+    }
+
+    if(games[0].screenshots)
+    {
+      embed
+        .setImage(`https://images.igdb.com/igdb/image/upload/t_thumb_2x/${games[0].screenshots[0].image_id}.jpg`);
+    }
+
+  interaction.reply({
+    embeds: [embed],
+  });
 }
---
+++ src/events/interactionCreate/findGameQuery.js
@@ -0,0 +1,31 @@
+const {Client, Interaction} = require('discord.js');
+
+
+const QUANTITY_OF_RESULTS = 6;
+
+  /**
+   *  @param {Client} client
+   *  @param {Interaction} interaction
+  */
+module.exports = async (client, interaction) => {
+  try {
+    if(!interaction.isAutocomplete()) return;
+    if(interaction.commandName !== 'game') return;
+    if(interaction.options.getSubcommand() !== 'search') return;
+
+    const req = await fetch(`https://api.igdb.com/v4/games`, {
+      method: 'POST',
+      headers: {
+        'Client-ID': process.env.IGDB_CLIENT_ID,
+        'Authorization': `Bearer ${client.igdb.access_token}`
+      },
+      body: `fields name, id; limit ${QUANTITY_OF_RESULTS}; search "${(interaction.options.getFocused() || "").replace('"', '\\"')}";`
+    });
+
+    const games = await req.json();
+
+    interaction.respond((games || []).map(game => {return {name: game.name, value: game.id}} )).catch(() => {});
+  } catch (e) {
+    console.log(e);
+  }
+}
