cbd2fb827811970051377dd6cbb309628c976a57
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-02-22 19:20:00 -0300

    Prevencao de logs de erro ao colar link de musica

--- src/events/interactionCreate/autocompletes/songQuery.js
+++ src/events/interactionCreate/autocompletes/songQuery.js
@@ -17,9 +17,9 @@ module.exports = async (client, interaction) => {
       let results = await player.search(value, {
         requestedBy: interaction.user,
         searchEngine: QueryType.AUTO
-      });
+      }).catch(() => {});
 
-      if(results.tracks)
+      if(results?.tracks)
       {
         let options = results.tracks.map(track => { return {name: track.title, value: track.url }});
         interaction.respond(options.slice(0, 25)).catch(() => {});
