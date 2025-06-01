6e8e8ef08bc61b297d5ece99f404f0599e00fe21
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-06-01 13:54:58 -0300

    Adicionando comando de shuffle da playlist

---
+++ src/commands/music/shuffle.js
@@ -0,0 +1,36 @@
+const { Client, Interaction, EmbedBuilder } = require('discord.js');
+const { useQueue } = require('discord-player')
+
+module.exports =  {
+  name: 'shuffle',
+  description: 'shuffles the current playlist',
+  /**
+   *  @param {Client} client
+   *  @param {Interaction} interaction
+  */
+  callback: async (client, interaction) => {
+    await interaction.deferReply({ ephemeral: true });
+
+    const queue = useQueue(interaction.guild);
+
+    if(!queue || queue.isEmpty())
+    {
+      await interaction.editReply({
+        embeds: [new EmbedBuilder().setDescription("There is no song to shuffle.")],
+      });
+      return;
+    } else if (queue.tracks.size < 2)
+    {
+      await interaction.editReply({
+        embeds: [new EmbedBuilder().setDescription("There is not enough tracks to shuffle.")],
+      });
+    }
+
+    queue.tracks.shuffle();
+
+    await interaction.editReply({
+      embeds: [new EmbedBuilder().setDescription(`:arrows_clockwise: Shuffled ${queue.tracks.size} tracks`)],
+    });
+  }
+
+}
