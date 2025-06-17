450d1e2fb80bb54fe5a6a73ce9c9821f7165e2aa
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-06-17 17:51:51 -0300

    Adicionando I18n paliativo para eventos do discord-player

--- src/utils/playerEvents/emptyChannel.js
+++ src/utils/playerEvents/emptyChannel.js
@@ -1,6 +1,9 @@
 const { EmbedBuilder } = require('discord.js');
 const { GuildQueue, Track } = require('discord-player');
 
+const { getI18n } = require("../../utils/i18n");
+const getLocalization = locale => require(`../../i18n/${getI18n(locale)}/playerEvents/emptyChannel`);
+
 module.exports = {
   name: 'emptyChannel',
   /**
@@ -10,10 +13,12 @@ module.exports = {
   callback: (queue, track) => {
 
     if (queue && !queue.deleted) {
+      const words = getLocalization(queue.metadata.preferredLocale);
+      
       queue.delete();
 
       queue.metadata.channel.send({
-        embeds: [new EmbedBuilder().setDescription(":broken_heart: Leaving the voice channel because i'm alone and sad, goodbye!")],
+        embeds: [new EmbedBuilder().setDescription(`:broken_heart: ${words.LeavingVC}`)],
       });
     }
   } 
--- src/utils/playerEvents/playerStart.js
+++ src/utils/playerEvents/playerStart.js
@@ -1,6 +1,9 @@
 const { EmbedBuilder } = require('discord.js');
 const { GuildQueue, Track } = require('discord-player');
 
+const { getI18n } = require("../../utils/i18n");
+const getLocalization = locale => require(`../../i18n/${getI18n(locale)}/playerEvents/playerStart`);
+
 module.exports = {
   name: 'playerStart',
 
@@ -9,11 +12,13 @@ module.exports = {
    * @param {Track} track 
   */
   callback: (queue, track) => {
+    const words = getLocalization(queue.metadata.preferredLocale);
+
     const embed = new EmbedBuilder()
     .setTitle(track.title)
     .setDescription(queue.currentTrack.author)
     .setThumbnail(track.thumbnail)
-    .setFooter({text: `duration: ${track.duration}`})
+    .setFooter({text: `${words.Duration}: ${track.duration}`})
     .setURL(track.url);
 
     queue.metadata.channel.send({embeds: [embed]});
