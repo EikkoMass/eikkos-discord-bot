711631dfaefe17da72288c7a8f42d76fdadf5fee
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-06-29 17:08:51 -0300

    Adicionando I18n ao /pause

--- src/commands/music/pause.js
+++ src/commands/music/pause.js
@@ -1,6 +1,8 @@
 const { Client, Interaction, EmbedBuilder } = require('discord.js');
 
-const { useQueue } = require('discord-player')
+const { useQueue } = require('discord-player');
+const { getI18n } = require("../../utils/i18n");
+const getLocalization = locale => require(`../../i18n/${getI18n(locale)}/pause`);
 
 module.exports =  {
   name: 'pause',
@@ -10,14 +12,25 @@ module.exports =  {
    *  @param {Interaction} interaction
   */
   callback: async (client, interaction) => {
+    
+    const words = getLocalization(interaction.locale);
+
     await interaction.deferReply({ ephemeral: true });
 
     const queue = useQueue(interaction.guild);
     const embed = new EmbedBuilder();
 
+    if(!queue?.node)
+    {
+      await interaction.editReply({
+        embeds: [embed.setDescription(":x: " + words.NoQueue)],
+      });
+      return;
+    }
+
     if (queue.node.isPaused()) {
       await interaction.editReply({
-        embeds: [embed.setDescription(":warning: The playback is already paused.")],
+        embeds: [embed.setDescription(":warning: " + words.AlreadyPaused)],
       });
       return;
     }
@@ -25,7 +38,7 @@ module.exports =  {
     queue.node.pause();
 
     await interaction.editReply({
-      embeds: [embed.setDescription(":ice_cube: Paused the playback! Use the command `/resume` to play again.")],
+      embeds: [embed.setDescription(":ice_cube: " + words.Paused)],
     });
   }
 
