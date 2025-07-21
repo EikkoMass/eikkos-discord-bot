cf50644aa787d7d28b41e98446e9f9b619101506
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-07-21 20:23:35 -0300

    Adicionando I18n ao /stop

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -2,6 +2,9 @@ const { Client, Interaction, EmbedBuilder, MessageFlags } = require('discord.js'
 
 const { useQueue } = require('discord-player')
 
+const { getI18n } = require("../../utils/i18n");
+const getLocalization = locale => require(`../../i18n/${getI18n(locale)}/stop`);
+
 module.exports =  {
   name: 'stop',
   description: 'stop the song on the voice channel',
@@ -10,19 +13,21 @@ module.exports =  {
    *  @param {Interaction} interaction
   */
   callback: async (client, interaction) => {
+    const words = getLocalization(interaction.locale);
+
     await interaction.deferReply({ flags: [ MessageFlags.Ephemeral ] });
 
     const queue = useQueue(interaction.guild);
     const embed = new EmbedBuilder();
 
     if (queue.isPlaying()) 
-      {
-        queue.node.stop();
-        await interaction.editReply({ embeds: [embed.setDescription(":rock: Stopped the song! Queue cleaned.")] });
-        return; 
-      }
+    {
+      queue.node.stop();
+      await interaction.editReply({ embeds: [embed.setDescription(`:rock: ${words.Stopped}`)] });
+      return; 
+    }
 
-      await interaction.editReply({ embeds: [embed.setDescription(":warning: No song are playing right now!")] });
+      await interaction.editReply({ embeds: [embed.setDescription(`:warning: ${words.NoSongPlaying}`)] });
   }
 
 }
