af6add95ad4374fb63321be2f5a0cdcfa7db3585
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-07-22 20:19:23 -0300

    Adicionando I18n para o botao stop do player

--- src/events/interactionCreate/buttonActions/player/stopSongResponse.js
+++ src/events/interactionCreate/buttonActions/player/stopSongResponse.js
@@ -1,26 +1,36 @@
 const { Client, Interaction, MessageFlags } = require('discord.js');
 const { useQueue } = require('discord-player');
 
+const { getI18n } = require("../../../../utils/i18n");
+const getLocalization = locale => require(`../../../../i18n/${getI18n(locale)}/stop`);
+
 /**
  *  @param {Client} client
  *  @param {Interaction} interaction
 */
 module.exports = async (client, interaction) => {
   try {
-      if(!interaction.isButton()) return;
-      if(!interaction.customId?.startsWith('player;')) return;
-      if(!interaction.customId.includes('stop;')) return;
+    if(!interaction.isButton()) return;
+    if(!interaction.customId?.startsWith('player;')) return;
+    if(!interaction.customId.includes('stop;')) return;
 
-      await interaction.deferReply({ 
-        flags: [ MessageFlags.Ephemeral ],
-      }) ;
-    
-      const queue = useQueue(interaction.guild);
+    const words = getLocalization(interaction.locale);
+
+    await interaction.deferReply({ 
+      flags: [ MessageFlags.Ephemeral ],
+    }) ;
   
+    const queue = useQueue(interaction.guild);
+
+    if (queue.isPlaying()) 
+    {
       queue.node.stop();
-      await interaction.editReply(`Stopped the song.`);
-  
-    } catch (err) {
-        console.log(err);
+      await interaction.editReply(`:rock: ${words.Stopped}`); 
     }
+
+    await interaction.editReply({ embeds: [embed.setDescription(`:warning: ${words.NoSongPlaying}`)] });
+
+  } catch (err) {
+      console.log(err);
+  }
 }
