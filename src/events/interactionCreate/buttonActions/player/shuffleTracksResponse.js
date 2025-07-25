bf72decc23f2eba6faedbcc301fd2f001453b0d4
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-07-25 01:33:21 -0300

    Adicionando I18n ao botao shuffle do player

--- src/events/interactionCreate/buttonActions/player/shuffleTracksResponse.js
+++ src/events/interactionCreate/buttonActions/player/shuffleTracksResponse.js
@@ -1,6 +1,9 @@
 const { Client, Interaction, MessageFlags, EmbedBuilder } = require('discord.js');
 const { useQueue } = require('discord-player');
 
+const { getI18n } = require("../../../../utils/i18n");
+const getLocalization = locale => require(`../../../../i18n/${getI18n(locale)}/shuffle`);
+
 /**
  *  @param {Client} client
  *  @param {Interaction} interaction
@@ -11,17 +14,33 @@ module.exports = async (client, interaction) => {
       if(!interaction.customId?.startsWith('player;')) return;
       if(!interaction.customId.includes('shuffle;')) return;
 
+      const words = getLocalization(interaction.locale);
+
       await interaction.deferReply({ 
         flags: [ MessageFlags.Ephemeral ],
       }) ;
     
       const queue = useQueue(interaction.guild);
   
+      if(!queue || queue.isEmpty())
+      {
+        await interaction.editReply({
+          embeds: [new EmbedBuilder().setDescription(words.NoSong)],
+        });
+        return;
+      } else if (queue.tracks.size < 2)
+      {
+        await interaction.editReply({
+          embeds: [new EmbedBuilder().setDescription(words.NotEnoughTracks)],
+        });
+      }
+
       queue.tracks.shuffle();
+
       await interaction.editReply({
-        embeds: [new EmbedBuilder().setDescription(`:arrows_clockwise: Shuffled ${queue.tracks.size} tracks`)]
+        embeds: [new EmbedBuilder().setDescription(`:arrows_clockwise: ${formatMessage(words.Shuffled, [queue.tracks.size])}`)],
       });
-  
+      
     } catch (err) {
         console.log(err);
     }
