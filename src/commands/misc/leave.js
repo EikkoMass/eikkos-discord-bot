635eecadc43e8df81c256dbd8a5ae036b10c81e2
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-08-02 15:40:40 -0300

    Adicionando  I18n para o comando /leave

--- src/commands/misc/leave.js
+++ src/commands/misc/leave.js
@@ -1,6 +1,9 @@
-const { Client, Interaction, EmbedBuilder } = require('discord.js');
+const { Client, Interaction, EmbedBuilder, MessageFlags } = require('discord.js');
 
-const { useQueue } = require('discord-player')
+const { useQueue } = require('discord-player');
+
+const { getI18n, formatMessage } = require("../../utils/i18n");
+const getLocalization = locale => require(`../../i18n/${getI18n(locale)}/leave`);
 
 module.exports =  {
   name: 'leave',
@@ -11,13 +14,24 @@ module.exports =  {
   */
   callback: (client, interaction) => {
 
+    const words = getLocalization(interaction.locale);
+
     const queue = useQueue(interaction.guild);
     const embed = new EmbedBuilder();
 
-    if (queue && !queue.deleted) queue.delete();
-
+    if(!queue || queue.deleted)
+    {
+      interaction.reply({
+        embeds: [embed.setDescription(words.NotInVC)],
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+    
+    queue.delete();
     interaction.reply({
-      embeds: [embed.setDescription("Leaving the voice channel!")]
+      embeds: [embed.setDescription(formatMessage(words.LeavingVC, [interaction.guild.members.me?.voice?.channel]))],
+      flags: MessageFlags.Ephemeral,
     });
   }
 
