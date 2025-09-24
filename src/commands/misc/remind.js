1b2748714bc23dd4f8b04a5323a7cc9f18c3299f
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-09-24 00:12:23 -0300

    Aplicando ping por fora do embed para notificar no comando remind

--- src/commands/misc/remind.js
+++ src/commands/misc/remind.js
@@ -1,33 +1,37 @@
-import {Client, ApplicationCommandOptionType, EmbedBuilder, MessageFlags } from 'discord.js';
-import ms from 'ms';
+import {
+  Client,
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  MessageFlags,
+} from "discord.js";
+import ms from "ms";
 
 import { getLocalization, formatMessage } from "../../utils/i18n.js";
 
 const remindersCache = {};
 
-export default  {
-  name: 'reminder',
-  description: 'Reminds you something later.',
+export default {
+  name: "reminder",
+  description: "Reminds you something later.",
   /**
    *  @param {Client} client
    *  @param  interaction
-  */
+   */
   callback: async (client, interaction) => {
-    switch(interaction.options.getSubcommand())
-    {
-      case 'create':
+    switch (interaction.options.getSubcommand()) {
+      case "create":
         await create(client, interaction);
         break;
-      case 'status':
+      case "status":
         await status(client, interaction);
         break;
-      case 'cancel':
+      case "cancel":
         await cancel(client, interaction);
         break;
       default:
         await interaction.reply({
-          flags: [ MessageFlags.Ephemeral ],
-          content: `Reminder command not found!`
+          flags: [MessageFlags.Ephemeral],
+          content: `Reminder command not found!`,
         });
         return;
     }
@@ -35,137 +39,144 @@ export default  {
 
   options: [
     {
-      name: 'create',
-      description: 'creates a new reminder',
+      name: "create",
+      description: "creates a new reminder",
       type: ApplicationCommandOptionType.Subcommand,
       options: [
         {
-          name: 'time',
-          description: 'the time out to remind (30 minutes , 1 hour , 1 day)',
+          name: "time",
+          description: "the time out to remind (30 minutes , 1 hour , 1 day)",
           type: ApplicationCommandOptionType.String,
-          required: true
+          required: true,
         },
         {
-          name: 'message',
-          description: 'What you want to remind?',
+          name: "message",
+          description: "What you want to remind?",
           type: ApplicationCommandOptionType.String,
         },
         {
-          name: 'user',
-          description: 'Who will receive the message?',
+          name: "user",
+          description: "Who will receive the message?",
           type: ApplicationCommandOptionType.User,
-        }
-      ]
+        },
+      ],
     },
     {
-      name: 'status',
-      description: 'show a list of your current reminders',
+      name: "status",
+      description: "show a list of your current reminders",
       type: ApplicationCommandOptionType.Subcommand,
     },
     {
-      name: 'cancel',
-      description: 'Cancel an active reminder',
+      name: "cancel",
+      description: "Cancel an active reminder",
       type: ApplicationCommandOptionType.Subcommand,
       options: [
         {
-          name: 'id',
-          description: 'The id of the pending reminder',
+          name: "id",
+          description: "The id of the pending reminder",
           type: ApplicationCommandOptionType.String,
-          required: true
-        } 
-      ]
-    }
-  ]
-}
+          required: true,
+        },
+      ],
+    },
+  ],
+};
 
-async function create(client, interaction)
-{
+async function create(client, interaction) {
   const words = await getLocalization(interaction.locale, `remind`);
-      
-  let time = interaction.options.get('time')?.value;
-  const message = interaction.options.get('message')?.value || "";
-  const user = interaction.options.get('user')?.value;
+
+  let time = interaction.options.get("time")?.value;
+  const message = interaction.options.get("message")?.value || "";
+  const user = interaction.options.get("user")?.value;
   let receiver = interaction.member;
 
   time = time
-    .replaceAll(words.Seconds, 'seconds')
-    .replaceAll(words.Second, 'second')
+    .replaceAll(words.Seconds, "seconds")
+    .replaceAll(words.Second, "second")
 
-    .replaceAll(words.Minutes, 'minutes')
-    .replaceAll(words.Minute, 'minute')
-    
-    .replaceAll(words.Hours, 'hours')
-    .replaceAll(words.Hour, 'hour');
+    .replaceAll(words.Minutes, "minutes")
+    .replaceAll(words.Minute, "minute")
 
-  if(user)
-  {
+    .replaceAll(words.Hours, "hours")
+    .replaceAll(words.Hour, "hour");
+
+  if (user) {
     receiver = await interaction.guild.members.fetch(user);
   }
 
   const duration = ms(time);
-  const {default: prettyMs} = await import('pretty-ms');
+  const { default: prettyMs } = await import("pretty-ms");
 
-  const formattedDuration = prettyMs(duration, {verbose: true});
+  const formattedDuration = prettyMs(duration, { verbose: true });
 
   interaction.reply({
-    flags: [ MessageFlags.Ephemeral ],
-    content: formatMessage(words.Ping, [formattedDuration])
+    flags: [MessageFlags.Ephemeral],
+    content: formatMessage(words.Ping, [formattedDuration]),
   });
 
   const embed = new EmbedBuilder();
 
-  embed
-  .setDescription(`<@${receiver.id}>${' ' + message || ''}`)
-  .setFooter({ text: formatMessage(words.EventHistory, [formattedDuration]) })
-  
+  embed.setDescription(message || " ").setFooter({
+    iconURL: interaction.member.displayAvatarURL({ size: 256 }),
+    text: formatMessage(words.EventHistory, [
+      interaction.member.displayName || interaction.member.nickname,
+      formattedDuration,
+    ]),
+  });
+
   const cacheIdentifier = `${interaction.member.id}$${interaction.guild.id}`;
-  const generatedId = Math.random().toString(36).replace('0.', '');
+  const generatedId = Math.random().toString(36).replace("0.", "");
 
   (function () {
-    
     const timeoutId = setTimeout(() => {
       interaction.channel.send({
-        embeds: [embed]
+        content: `<@${receiver.id}>`,
+        embeds: [embed],
       });
-        
-      remindersCache[cacheIdentifier].splice(remindersCache[cacheIdentifier].findIndex(cache => cache.id === generatedId), 1);
+
+      remindersCache[cacheIdentifier].splice(
+        remindersCache[cacheIdentifier].findIndex(
+          (cache) => cache.id === generatedId,
+        ),
+        1,
+      );
     }, Number.parseInt(duration));
 
-    if(!Array.isArray(remindersCache[cacheIdentifier])) remindersCache[cacheIdentifier] = [];
+    if (!Array.isArray(remindersCache[cacheIdentifier]))
+      remindersCache[cacheIdentifier] = [];
     remindersCache[cacheIdentifier].push({
       id: generatedId,
       timeoutId,
       message,
-      receiver
-    })
+      receiver,
+    });
   })();
-
 }
 
-async function status(client, interaction)
-{
+async function status(client, interaction) {
   const words = await getLocalization(interaction.locale, `remind`);
-  
+
   const embeds = [];
   const cacheIdentifier = `${interaction.member.id}$${interaction.guild.id}`;
 
-  if(!Array.isArray(remindersCache[cacheIdentifier]))
-  {
+  if (
+    !Array.isArray(remindersCache[cacheIdentifier]) ||
+    !remindersCache[cacheIdentifier].length
+  ) {
     interaction.reply({
-      flags: [ MessageFlags.Ephemeral ],
+      flags: [MessageFlags.Ephemeral],
       embeds: [new EmbedBuilder().setDescription(words.NotFound)],
     });
     return;
   }
 
-  for(let reminder of remindersCache[cacheIdentifier]) {
+  for (let reminder of remindersCache[cacheIdentifier]) {
     const user = reminder.receiver.user;
     const embed = new EmbedBuilder()
-    .setTitle(`${words.To}: ${user.displayName || user.nickname}`)
-    .setFooter({ text: `ID: ${reminder.id}` });
+      .setTitle(`${words.To}: ${user.displayName || user.nickname}`)
+      .setFooter({ text: `ID: ${reminder.id}` });
 
-    if(reminder.message)
-    {
+    if (reminder.message) {
       embed.setDescription(reminder.message);
     }
 
@@ -173,43 +184,42 @@ async function status(client, interaction)
   }
 
   interaction.reply({
-    flags: [ MessageFlags.Ephemeral ],
-    embeds
+    flags: [MessageFlags.Ephemeral],
+    embeds,
   });
 }
 
-async function cancel(client, interaction)
-{
+async function cancel(client, interaction) {
   const words = await getLocalization(interaction.locale, `remind`);
 
-  let id = interaction.options.get('id')?.value;
+  let id = interaction.options.get("id")?.value;
   const cacheIdentifier = `${interaction.member.id}$${interaction.guild.id}`;
 
-  if(!Array.isArray(remindersCache[cacheIdentifier]))
-    {
-      interaction.reply({
-        flags: [ MessageFlags.Ephemeral ],
-        embeds: [new EmbedBuilder().setDescription(words.NotFound)],
-      });
-      return;
-    }
-
-    let index = remindersCache[cacheIdentifier].findIndex(cache => cache.id === id);
-
-    if(index < 0)
-    {
-      interaction.reply({
-        flags: [ MessageFlags.Ephemeral ],
-        embeds: [new EmbedBuilder().setDescription(words.NotFoundSpecified)],
-      });
-      return;
-    }
-
-    clearTimeout(remindersCache[cacheIdentifier][index].timeoutId);
-    remindersCache[cacheIdentifier].splice(index, 1);
-
+  if (!Array.isArray(remindersCache[cacheIdentifier])) {
     interaction.reply({
-      flags: [ MessageFlags.Ephemeral ],
-      embeds: [new EmbedBuilder().setDescription(words.Cancelled)],
+      flags: [MessageFlags.Ephemeral],
+      embeds: [new EmbedBuilder().setDescription(words.NotFound)],
     });
+    return;
+  }
+
+  let index = remindersCache[cacheIdentifier].findIndex(
+    (cache) => cache.id === id,
+  );
+
+  if (index < 0) {
+    interaction.reply({
+      flags: [MessageFlags.Ephemeral],
+      embeds: [new EmbedBuilder().setDescription(words.NotFoundSpecified)],
+    });
+    return;
+  }
+
+  clearTimeout(remindersCache[cacheIdentifier][index].timeoutId);
+  remindersCache[cacheIdentifier].splice(index, 1);
+
+  interaction.reply({
+    flags: [MessageFlags.Ephemeral],
+    embeds: [new EmbedBuilder().setDescription(words.Cancelled)],
+  });
 }
