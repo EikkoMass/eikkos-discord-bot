193215816406413cff401a99b6eed8aeeed7025c
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-10-02 08:35:24 -0300

    Adicionando i18n ao comando disconnect

--- src/commands/voice/disconnect.js
+++ src/commands/voice/disconnect.js
@@ -7,6 +7,8 @@ import {
 } from "discord.js";
 import ms from "ms";
 
+import { getLocalization, formatMessage } from "../../utils/i18n.js";
+
 export default {
   name: "disconnect",
   description: "Disconnects the user from the voice channel",
@@ -15,16 +17,29 @@ export default {
    *  @param  interaction
    */
   callback: async (client, interaction) => {
+    const words = await getLocalization(interaction.locale, `disconnect`);
     const embed = new EmbedBuilder();
+    const INSTANT = "0 seconds";
 
-    let timer = interaction.options.get("timer")?.value || "0 seconds";
+    let timer = interaction.options.get("timer")?.value || INSTANT;
     let member = interaction.options.get("user")?.value;
-
-    console.log(member);
-    console.log(timer);
 
     if (member) {
       member = await interaction.guild.members.fetch(member);
+    } else {
+      member = interaction.member;
+    }
+
+    if (timer) {
+      timer = timer
+        .replaceAll(words.Seconds, "seconds")
+        .replaceAll(words.Second, "second")
+
+        .replaceAll(words.Minutes, "minutes")
+        .replaceAll(words.Minute, "minute")
+
+        .replaceAll(words.Hours, "hours")
+        .replaceAll(words.Hour, "hour");
     }
 
     if (
@@ -34,7 +49,7 @@ export default {
     ) {
       interaction.reply({
         flags: [MessageFlags.Ephemeral],
-        embeds: [embed.setDescription("Missing permissions")],
+        embeds: [embed.setDescription(words.MissingPermissions)],
       });
       return;
     }
@@ -42,7 +57,7 @@ export default {
     if (!member?.voice?.channel) {
       interaction.reply({
         flags: [MessageFlags.Ephemeral],
-        embeds: [embed.setDescription("Must be in voice channel")],
+        embeds: [embed.setDescription(words.MustBeInVC)],
       });
       return;
     }
@@ -54,7 +69,16 @@ export default {
 
     interaction.reply({
       flags: [MessageFlags.Ephemeral],
-      embeds: [embed.setDescription("Disconnecting...")],
+      embeds: [
+        embed.setDescription(
+          timer === INSTANT
+            ? formatMessage(words.DisconnectingUser, [member.id])
+            : formatMessage(words.DisconnectingUserWithDuration, [
+                member.id,
+                timer,
+              ]),
+        ),
+      ],
     });
 
     setTimeout(() => {
@@ -67,7 +91,7 @@ export default {
       name: "user",
       description: "the user you want to disconnect",
       type: ApplicationCommandOptionType.User,
-      required: true,
+      required: false,
     },
     {
       name: "timer",
