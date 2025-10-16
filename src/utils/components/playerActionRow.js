c50b32d6795bb75981be8290d42a196ef704aeaf
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-10-16 16:51:59 -0300

    Ajustando actionRows do player para layout novo de customId

--- src/utils/components/playerActionRow.js
+++ src/utils/components/playerActionRow.js
@@ -5,46 +5,72 @@ import { ActionRowBuilder, ButtonStyle, ButtonBuilder } from "discord.js";
  */
 export default () => {
   const row = new ActionRowBuilder();
+  const hash = crypto.randomUUID();
 
   row.components.push(
     new ButtonBuilder()
-      .setCustomId(`player;pause;`)
+      .setCustomId(
+        JSON.stringify({
+          id: `player;pause;`,
+          hash,
+        }),
+      )
       .setEmoji("<:pause:1391131987195985941>")
-      .setLabel(' ')
-      .setStyle(ButtonStyle.Secondary)
-  )
+      .setLabel(" ")
+      .setStyle(ButtonStyle.Secondary),
+  );
 
   row.components.push(
     new ButtonBuilder()
-      .setCustomId(`player;play;`)
+      .setCustomId(
+        JSON.stringify({
+          id: `player;play;`,
+          hash,
+        }),
+      )
       .setEmoji("<:play:1391132094230429736>")
-      .setLabel(' ')
-      .setStyle(ButtonStyle.Secondary)
-  )
+      .setLabel(" ")
+      .setStyle(ButtonStyle.Secondary),
+  );
 
   row.components.push(
     new ButtonBuilder()
-      .setCustomId(`player;skip;`)
+      .setCustomId(
+        JSON.stringify({
+          id: `player;skip;`,
+          hash,
+        }),
+      )
       .setEmoji("<:skip_right:1391132023745286296>")
-      .setLabel(' ')
-      .setStyle(ButtonStyle.Secondary)
-  )
-
-  row.components.push(
-  new ButtonBuilder()
-    .setCustomId(`player;shuffle;`)
-    .setEmoji("<:shuffle:1394480011573985391>")
-    .setLabel(' ')
-    .setStyle(ButtonStyle.Secondary)
-  )
+      .setLabel(" ")
+      .setStyle(ButtonStyle.Secondary),
+  );
 
   row.components.push(
     new ButtonBuilder()
-      .setCustomId(`player;stop;`)
+      .setCustomId(
+        JSON.stringify({
+          id: `player;shuffle;`,
+          hash,
+        }),
+      )
+      .setEmoji("<:shuffle:1394480011573985391>")
+      .setLabel(" ")
+      .setStyle(ButtonStyle.Secondary),
+  );
+
+  row.components.push(
+    new ButtonBuilder()
+      .setCustomId(
+        JSON.stringify({
+          id: `player;stop;`,
+          hash,
+        }),
+      )
       .setEmoji("<:stop:1391132125281124362>")
-      .setLabel(' ')
-      .setStyle(ButtonStyle.Secondary)
-  )
+      .setLabel(" ")
+      .setStyle(ButtonStyle.Secondary),
+  );
 
   return row;
-}
+};
