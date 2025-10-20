20473893c166677ca2f2fc9319d050c7ab2aa125
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-10-20 15:36:47 -0300

    Adicionando randomizacao por seed no comando affinity

--- src/commands/misc/affinity.js
+++ src/commands/misc/affinity.js
@@ -1,21 +1,25 @@
-import {Client, ApplicationCommandOptionType, MessageFlags, EmbedBuilder } from 'discord.js';
+import {
+  Client,
+  ApplicationCommandOptionType,
+  MessageFlags,
+  EmbedBuilder,
+} from "discord.js";
 
-export default  {
-  name: 'affinity',
-  description: 'Calculate the affinity (love / hate) between 2 users',
+export default {
+  name: "affinity",
+  description: "Calculate the affinity (love / hate) between 2 users",
   callback: async (client, interaction) => {
-    switch(interaction.options.getSubcommand())
-    {
-      case 'love':
+    switch (interaction.options.getSubcommand()) {
+      case "love":
         await love(client, interaction);
         break;
-      case 'hate':
+      case "hate":
         await hate(client, interaction);
         break;
       default:
         await interaction.reply({
-          flags: [ MessageFlags.Ephemeral ],
-          content: `Affinity command not found!`
+          flags: [MessageFlags.Ephemeral],
+          content: `Affinity command not found!`,
         });
         return;
     }
@@ -23,109 +27,121 @@ export default  {
 
   options: [
     {
-      name: 'love',
-      description: 'How much love they share',
+      name: "love",
+      description: "How much love they share",
       type: ApplicationCommandOptionType.Subcommand,
       options: [
         {
-          name: 'first',
-          description: 'The first user you want to match',
+          name: "first",
+          description: "The first user you want to match",
           type: ApplicationCommandOptionType.User,
-          required: true
-        },    
+          required: true,
+        },
         {
-          name: 'second',
-          description: 'The second user you want to match',
+          name: "second",
+          description: "The second user you want to match",
           type: ApplicationCommandOptionType.User,
-          required: true
-        }
-      ]
+          required: true,
+        },
+      ],
     },
     {
-      name: 'hate',
-      description: 'How much they want to crush each other apart',
+      name: "hate",
+      description: "How much they want to crush each other apart",
       type: ApplicationCommandOptionType.Subcommand,
       options: [
         {
-          name: 'first',
-          description: 'The first user you want to match',
+          name: "first",
+          description: "The first user you want to match",
           type: ApplicationCommandOptionType.User,
-          required: true
-        },    
+          required: true,
+        },
         {
-          name: 'second',
-          description: 'The second user you want to match',
+          name: "second",
+          description: "The second user you want to match",
           type: ApplicationCommandOptionType.User,
-          required: true
-        }
-      ]
-    }
-  ]
-}
+          required: true,
+        },
+      ],
+    },
+  ],
+};
 
 /**
  *  @param {Client} client
  *  @param  interaction
-*/
+ */
 async function love(client, interaction) {
-  
-  const user1 = interaction.options?.get('first').value;
-  const user2 = interaction.options?.get('second').value;
+  const user1 = interaction.options?.get("first").value;
+  const user2 = interaction.options?.get("second").value;
 
-  const results = affinityCalc('love');
+  const results = affinityCalc("love", user1, user2);
 
   let embed = new EmbedBuilder()
-  .setTitle("Love percentage")
-  .setDescription(`<@${user1}> level of love with <@${user2}> is : ${results.percentage}%\n\n${results.level}`)
+    .setTitle("Love percentage")
+    .setDescription(
+      `<@${user1}> level of love with <@${user2}> is : ${results.percentage}%\n\n${results.level}`,
+    );
 
   interaction.reply({
-    embeds:[embed]
+    embeds: [embed],
   });
 }
 
 /**
  *  @param {Client} client
  *  @param  interaction
-*/
+ */
 async function hate(client, interaction) {
-  
-  const user1 = interaction.options?.get('first').value;
-  const user2 = interaction.options?.get('second').value;
+  const user1 = interaction.options?.get("first").value;
+  const user2 = interaction.options?.get("second").value;
 
-  const results = affinityCalc('hate');
+  const results = affinityCalc("hate", user1, user2);
 
   let embed = new EmbedBuilder()
-  .setTitle("Hate percentage")
-  .setDescription(`<@${user1}> level of hate with <@${user2}> is : ${results.percentage}%\n\n${results.level}`)
+    .setTitle("Hate percentage")
+    .setDescription(
+      `<@${user1}> level of hate with <@${user2}> is : ${results.percentage}%\n\n${results.level}`,
+    );
 
   interaction.reply({
-    embeds:[embed]
+    embeds: [embed],
   });
 }
 
-
-function affinityCalc(type) {
-  
+function affinityCalc(type, user1, user2) {
   const affinityEmojis = {
     love: {
       filled: ":heart:",
-      empty: ":black_heart:"
+      empty: ":black_heart:",
     },
     hate: {
       filled: ":broken_heart:",
-      empty: ":black_heart:"
-    }
-  }
-  
+      empty: ":black_heart:",
+    },
+  };
+
   const emojis = affinityEmojis[type];
 
-  const affinity = Math.round(Math.random() * 100);
-  const affinityIndex = Math.floor(affinity / 10);
-  const affinityLevel = emojis.filled.repeat(affinityIndex) + emojis.empty.repeat(10 - affinityIndex);
-  
+  let affinity = Math.round(randomize(user1 + user2) * 100);
+
+  if (type === "hate") {
+    affinity = 100 - affinity;
+  }
+
+  let affinityIndex = Math.floor(affinity / 10);
+  let affinityLevel =
+    emojis.filled.repeat(affinityIndex) +
+    emojis.empty.repeat(10 - affinityIndex);
+
   return {
     percentage: affinity,
     index: affinityIndex,
-    level: affinityLevel
-  }
+    level: affinityLevel,
+  };
+}
+
+function randomize(seed) {
+  var x = Math.sin(seed || 1) * 10000;
+  return x - Math.floor(x);
 }
