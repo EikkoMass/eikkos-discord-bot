de684fb4a1aa15c263f51bcbbb6d9fed860f892f
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-11-22 15:45:24 -0300

    Adicionando i18n para o comando question

--- src/commands/misc/question.js
+++ src/commands/misc/question.js
@@ -1,40 +1,43 @@
-import { Client, ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import { Client, ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+
+import { getLocalization } from "../../utils/i18n.js";
 
 export default {
+  name: "question",
+  description: "Create a question with emoji voting",
 
-  name: 'question',
-  description: 'Create a question with emoji voting',
-
-  /** 
-   * 
+  /**
+   *
    *  @param {Client} client
    *  @param  interaction
-  */
+   */
   callback: async (client, interaction) => {
-    const text = interaction.options.get('text').value;
+    const words = await getLocalization(interaction.locale, "question");
+    const text = interaction.options.get("text").value;
 
     await interaction.deferReply();
     await interaction.deleteReply();
 
     const message = await interaction.channel.send({
-        embeds: [
-          new EmbedBuilder()
-            .setFields([ {name: 'requester', value: `<@${interaction.member.id}>`}])
-            .setTitle(text)
-        ]
-      });
+      embeds: [
+        new EmbedBuilder()
+          .setFields([
+            { name: words.Requester, value: `<@${interaction.member.id}>` },
+          ])
+          .setTitle(text),
+      ],
+    });
 
-
-    await message.react('👍');
-    await message.react('👎');
+    await message.react("👍");
+    await message.react("👎");
   },
 
   options: [
     {
-      name: 'text',
-      description: 'What you want to ask?',
-      type: ApplicationCommandOptionType.String, 
-      required: true
-    }
-  ]
-}
+      name: "text",
+      description: "What you want to ask?",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
+};
