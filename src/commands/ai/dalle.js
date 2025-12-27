4dcf1a7d5794d15a6cef7be88cdb8464896ce76a
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-12-27 02:37:41 -0300

    Melhorando exibicao de imagem retornada pelo dalle

--- src/commands/ai/dalle.js
+++ src/commands/ai/dalle.js
@@ -1,65 +1,74 @@
-import {Client, ApplicationCommandOptionType, MessageFlags} from 'discord.js';
-import OpenAI from 'openai';
-import dotenv from 'dotenv';
+import { Client, ApplicationCommandOptionType, MessageFlags } from "discord.js";
+import OpenAI from "openai";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export default  {
-  name: 'dalle',
-  description: 'send a prompt to openai\'s dalle',
+export default {
+  name: "dalle",
+  description: "send a prompt to openai's dalle",
+  options: [
+    {
+      name: "prompt",
+      description: "the prompt you want to generate",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
   /**
    *  @param {Client} client
    *  @param  interaction
-  */
+   */
   callback: async (client, interaction) => {
-
     const px = 1024;
-    const prompt = interaction.options.get('prompt')?.value;
+    const prompt = interaction.options.get("prompt")?.value;
 
-    if(!prompt)
-    {
+    if (!prompt) {
       interaction.reply({
-        flags: [ MessageFlags.Ephemeral ],
-        content: 'You need to send an valid input'
+        flags: [MessageFlags.Ephemeral],
+        content: "You need to send an valid input",
       });
       return;
     }
 
-    if(!process.env.OPENAI_API_KEY)
-    {
+    if (!process.env.OPENAI_API_KEY) {
       interaction.reply({
-        flags: [ MessageFlags.Ephemeral ],
-        content: 'Missing API key'
+        flags: [MessageFlags.Ephemeral],
+        content: "Missing API key",
       });
       return;
     }
 
     interaction.reply({
-      flags: [ MessageFlags.Ephemeral ],
-      content: `Generating the image, please wait!`
+      flags: [MessageFlags.Ephemeral],
+      content: `Generating the image, please wait!`,
     });
-    
-    const response = await (new OpenAI({ OPENAI_API_KEY: process.env.OPENAI_API_KEY })).images.generate({
+
+    const response = await new OpenAI({
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    }).images.generate({
       model: "dall-e-3",
       prompt,
       n: 1,
       size: `${px}x${px}`,
     });
-    
-    
-    if(response) {
-      interaction.channel.send(response.data[0].url);
-    } else {
-      throw new Error(response);
-    }
 
-  },
-  options: [
-    {
-      name: 'prompt',
-      description: 'the prompt you want to generate',
-      type: ApplicationCommandOptionType.String,
-      required: true
+    if (response) {
+      const embed = new EmbedBuilder()
+        .setTitle(`OpenAI generated image`)
+        .setDescription(`Prompt: \`${prompt}\``)
+        .setColor(Colors.Blurple)
+        .setImage(response.data[0].url)
+        .setFooter({
+          text: new Date().toDateString(),
+          iconURL: interaction.member.displayAvatarURL({ size: 256 }),
+        });
+
+      interaction.channel.send({
+        embeds: [embed],
+      });
+    } else {
+      console.log(response);
     }
-  ]
-}
+  },
+};
